31d94d0aa8739c3a3825c47ca1c11c8b2f7dcbb2
Wesam Gerges <rashad.wesam@gmail.com>
2019-09-15 19:22:32 -0400

    Add nuxt and dist files.

--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -138,7 +138,7 @@ export async function setContext(app, context) {
       payload: context.payload,
       error: context.error,
       base: '/',
-      env: {}
+      env: {"baseUrl":"http://localhost:3000","apiBaseUrl":"http://127.0.0.1:3333/api/v1"}
     }
     // Only set once
     if (context.req) {

fb892d037771097a54fee917ea159167d4a4eb95
Wesam Gerges <rashad.wesam@gmail.com>
2019-09-14 20:23:35 -0400

    Add nav menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,41 @@
 <template>
   <div class='container'>
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="primary">
+        <b-navbar-brand href="#">CanaPrep</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template v-slot:button-content>
+                User
+              </template>
+              <b-dropdown-item href="#" @click.prevent="logout">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>    
     <nuxt />
   </div>
 </template>
+
+<script>
+
+export default {
+  methods: {
+    async logout() {
+      await this.$auth.logout()
+    },
+  }
+}
+</script>
 
 <style>
 html {
